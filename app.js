0bbaacb716cfb481475eea02dfe4529168397946
enrique95 <enrique_alvarado95@hotmail.com>
2023-08-02 10:20:25 -0600

    removed function

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ import app from './src/app.js';
 
 const PORT = process.env.PORT;
 
-function startServer() {
-  app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-  });
-}
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
