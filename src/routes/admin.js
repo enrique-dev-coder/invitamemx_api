ad6a713efbe2d6db2d4a6db0f98282e1409cd162
Enrique <enrique_alvarado95@hotmail.com>
2023-08-03 12:45:15 -0600

    commneted policy field

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,40 +1,40 @@
-import { Router } from 'express';
-import { customAlphabet } from 'nanoid';
-import AWS from '../aws.js';
+import { Router } from "express";
+import { customAlphabet } from "nanoid";
+import AWS from "../aws.js";
 
 const adminRouter = Router();
 
 const dynamodb = new AWS.DynamoDB();
 
-adminRouter.get('/users', (req, res) => {
+adminRouter.get("/users", (req, res) => {
   const params = {
-    TableName: 'Usuarios', // Reemplaza con el nombre de tu tabla de usuarios
+    TableName: "Usuarios", // Reemplaza con el nombre de tu tabla de usuarios
   };
 
   // Realizar el escaneo de la tabla para obtener todos los usuarios
   dynamodb.scan(params, (err, data) => {
     if (err) {
-      console.error('Error al obtener los usuarios:', err);
-      return res.status(500).json({ error: 'Error interno del servidor' });
+      console.error("Error al obtener los usuarios:", err);
+      return res.status(500).json({ error: "Error interno del servidor" });
     } else {
       const usuarios = data.Items; // Los usuarios se encuentran en la propiedad Items del resultado
-      console.log('Usuarios obtenidos correctamente:', usuarios);
+      console.log("Usuarios obtenidos correctamente:", usuarios);
       return res.status(200).json(usuarios);
     }
   });
 });
 
-adminRouter.post('/user', (req, res) => {
+adminRouter.post("/user", (req, res) => {
   // Obtener los datos enviados desde el cliente (JSON en este caso)
-  const { nombre, email, cellphone, policyAccepted } = req.body;
+  const { nombre, email, cellphone } = req.body;
 
   // Validar que los datos requeridos estén presentes en la solicitud
   if (!nombre || !email) {
-    return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+    return res.status(400).json({ error: "Todos los campos son obligatorios" });
   }
 
   // Generar un ID único para el nuevo usuario
-  const alphabet = '0123456789';
+  const alphabet = "0123456789";
   const nanoid = customAlphabet(alphabet, 4);
   const id = nanoid();
 
@@ -45,28 +45,28 @@ adminRouter.post('/user', (req, res) => {
     email: { S: email },
     cellphone: { S: cellphone },
     // los booleanos se tienen que pasar a strings por cosas de dynamo db
-    policyAccepted: { S: policyAccepted.toString() },
+    // policyAccepted: { S: policyAccepted.toString() },
     //  password: { S: 'hashed_password' },
     // Puedes agregar otros atributos aquí si es necesario
   };
 
   // Parámetros para la operación de escritura en DynamoDB
   const putParams = {
-    TableName: 'Usuarios', // Reemplaza con el nombre de tu tabla de usuarios
+    TableName: "Usuarios", // Reemplaza con el nombre de tu tabla de usuarios
     Item: usuario,
-    ReturnValues: 'ALL_OLD',
+    ReturnValues: "ALL_OLD",
   };
 
   // Realizar la operación de escritura en DynamoDB para agregar el nuevo usuario
   dynamodb.putItem(putParams, (err, data) => {
     if (err) {
-      console.error('Error al agregar el usuario:', err);
-      return res.status(500).json({ error: 'Error interno del servidor' });
+      console.error("Error al agregar el usuario:", err);
+      return res.status(500).json({ error: "Error interno del servidor" });
     } else {
-      console.log('Usuario agregado correctamente:', data);
+      console.log("Usuario agregado correctamente:", data);
       return res
         .status(201)
-        .json({ message: 'Usuario agregado correctamente' });
+        .json({ message: "Usuario agregado correctamente" });
     }
   });
 });
