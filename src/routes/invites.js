50f61cefff0de32f935b3392920d523a5fd3b732
enrique95 <enrique_alvarado95@hotmail.com>
2023-08-04 10:12:50 -0600

    added invitations /name endpoint

--- src/routes/invites.js
+++ src/routes/invites.js
@@ -17,4 +17,28 @@ inviteRouter.get('/', async (req, res) => {
   );
 });
 
+inviteRouter.get('/:name', async (req, res) => {
+  const { name } = req.params;
+  // peticion desde wordpress
+  // end point personalizado con plugin
+  const response = await fetch(
+    `https://invitamemx.com/wp-json/wp/v2/forms/submissions/${name}`
+  );
+  const data = await response.json();
+  // devolver lo que necesito
+  return res.status(200).json(
+    data.map((invitado) => ({
+      id: invitado?.id,
+      link: invitado?.referer,
+      inviteName: invitado?.referer_title,
+      nombreInvitado: invitado.form_values?.name,
+      parentesco: invitado.form_values?.field_9a97d10,
+      acompañante: invitado.form_values?.field_e6ea7a9,
+      alergias: invitado.form_values?.message,
+      asistencia: invitado.form_values?.field_a440fe3,
+      telefono: invitado.form_values?.email,
+    }))
+  );
+});
+
 export default inviteRouter;
