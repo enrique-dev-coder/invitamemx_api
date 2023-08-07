c4ed99663e6d359b89e0b01c84e8c93071d966d1
enrique95 <enrique_alvarado95@hotmail.com>
2023-08-06 18:08:39 -0600

    added enpoint for auth/login

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import adminRouter from './routes/admin.js';
 import inviteRouter from './routes/invites.js';
+import authRouter from './routes/auth.js';
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(helmet());
 //routes
 app.use('/administration', adminRouter);
 app.use('/invitations', inviteRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.json('hola desde el server');
